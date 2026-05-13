943f6073976c12c341e821b8e71154f555317219
Kombo <zefirinkombo@email.com>
2026-05-13 23:17:10 +0100

    Miss à jour (3) (Socket.io) 13/05/2026

---
+++ Etat_File.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const file = path.join(__dirname, 'Etat.json');
+
+function LireEtat() {
+    const data = fs.readFileSync(file, 'utf-8');
+    return JSON.parse(data);
+}
+
+function EcrireEtat(etat) {
+    fs.writeFileSync(file, JSON.stringify(etat, null, 2), 'utf-8');
+}
+
+module.exports = {
+    LireEtat,
+    EcrireEtat
+};
--- app.js
+++ app.js
@@ -2,26 +2,32 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
+const {LireEtat, EcrireEtat } = require('./Etat_File')
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors : {origin: "*"} });
 
 io.on("connection", (socket) => {
-
-      console.log("client connected :", socket.id);
+        console.log("client connected :", socket.id);
+        const etat = LireEtat();
+        socket.emit("Etat", etat);
 
   socket.on("hello", (data) => {
-
-      console.log("Message : ",data); 
-    io.emit("hello", data);
+        console.log("Message : ",data); 
+        io.emit("hello", data);
+  });
+    
+  socket.on("Donner", (data) => {
+        EcrireEtat(data);
+        io.emit("Etat", data);
   });
 
   socket.on("disconnect", () => {
-      console.log("client disconnected :", socket.id);
+        console.log("client disconnected :", socket.id);
   });
 
 });
-
 httpServer.listen(3000, () => {
-    console.log("Server en écoute au port 3000");
+        console.log("Server en écoute au port 3000");
 });
