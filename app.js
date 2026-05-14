fbc090afb4919688d87bde45bb399c9c5e0d8cfc
Kombo <zefirinkombo@gmail.com>
2026-05-14 20:42:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ const io = new Server(httpServer, { cors : {origin: "*"} });
 io.on("connection", (socket) => {
         console.log("client connected :", socket.id);
         const etat = LireEtat();
-        socket.emit("Etat", etat);
+        socket.emit("Reponces", etat);
 
-  socket.on("hello", (data) => {
+  socket.on("Message", (data) => {
         console.log("Message : ",data); 
-        io.emit("hello", data);
+        io.emit("Requetes", data);
   });
-    
+  
+
   socket.on("Donner", (data) => {
         EcrireEtat(data);
         io.emit("Etat", data);
